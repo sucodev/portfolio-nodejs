687e238674e42a40bc61b48e9b6a34b8743f3822
Marcos Del Valle <agenciatomz@gmail.com>
2020-02-03 21:41:33 -0300

    Finalização do desafio 2-3, onde aplicamos rotas com o express e também o uso do template engine nunjucks

---
+++ readme.md
@@ -0,0 +1,5 @@
+[x] - Instalar o express
+    [] - Criar rotas ( About, Courses, page-not-found)
+[x] - Instalar o nodemon
+    [] - Substituir o "start": "node server.js" | "start": "nodemon server.js"
+[x] - Instalar o nunjucks 
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+
+// Usando uma função do express ( static ) que faz com que os meus arquivos estáticos que estão presentes dentro da pasta public sejam interpretados
+server.use(express.static('public'))
+
+// nesta função estou falando que todas as rotas serao 
+// interpretadas pelo arquivo render 
+server.set('view engine', 'html')
+
+// Todos os arquivos da views seja lidos pela interpretação
+nunjucks.configure('views', {
+    express:server
+})
+
+// Criação das rotas
+server.get("/", (req, res) => {
+    return res.render('about')
+})
+server.get("/courses", (req, res) => {
+    return res.render('courses')
+})
+server.use((req, res) => {
+    res.status(404).render('not-found')
+})
+
+//  Ele precisa ouvir a porta 3333 para rodar o servidor
+server.listen(3333, () => {
+    console.log('Server is running')
+})
